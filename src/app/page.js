1fa2941768d0efd33cddef6498e25c8f439e6387
silas martins <silas07official@gmail.com>
2024-03-17 15:52:42 +0100

    page, css and layout cleanup

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h2>Student Onboarding</h2>
+    <div className="bg-blue-50 min-h-screen p-4">
+      <div className="mx-auto bg-white w-full max-w-5xl border border-gray-100 rounded-lg shadow sm:p-4 md:p-6 dark:bg-gray-800 dark:border-gray-700 grid grid-cols-gap-4 min-h-screen">
+        hello
+      </div>
+    </div>
   );
 }
